31a0e0ad3c6f2379e09d2a101775ef6436225fa5
BRYAN JHONS GARCES <garcesbryan11@gmail.com>
2022-09-05 08:54:12 +0800

    saveCBC40%

--- MediCon/Controllers/LaboratoryResultController.cs
+++ MediCon/Controllers/LaboratoryResultController.cs
@@ -50,6 +50,39 @@ namespace MediCon.Controllers
                 return new HttpStatusCodeResult(HttpStatusCode.InternalServerError, ex.InnerException.Message);
             }
         }
+        public ActionResult saveCBC(CBC result)
+        {
+            try
+            {
+                if (!ModelState.IsValid)
+                {
+                    var temp = string.Join(" | ", ModelState.Values
+                     .SelectMany(v => v.Errors)
+                     .Select(e => e.ErrorMessage));
+                    return new HttpStatusCodeResult(HttpStatusCode.InternalServerError, "Model State Not Valid" + temp);
+                }
+                else
+                {
+                    // SAVE LAB RESULT OF BLOOD CHEM
+                    result.cbcID = generateID(result.labID).Substring(0, 15);
+                    result.dateTimeLog = DateTime.Now;
+                    result.personnelID = Session["personnelID"].ToString();
+                    db.CBCs.Add(result);
+
+                    // SAVE TAGGING OF TRUE TO ISENCODED FIELD (LABORATORY EXAM)
+                    var findLab = db.LaboratoryExams.SingleOrDefault(a => a.labID == result.labID);
+                    findLab.isEncoded = true;
+                    db.Entry(findLab).State = EntityState.Modified;
+
+                    db.SaveChanges();
+                }
+                return new HttpStatusCodeResult(HttpStatusCode.OK, "Successfully Save");
+            }
+            catch (Exception ex)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.InternalServerError, ex.InnerException.Message);
+            }
+        }
 
         public string generateID(string source)
         {
